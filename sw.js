81cb0d389195f052543736efd6f74d8eae850e0e
jeffvedd <jeffersonazevedo69@gmail.com>
2026-06-29 08:29:52 +0000

    feat: add PWA support with manifest, service worker and icons

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'wifiscope-v1';
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/WifiScope/', '/WifiScope/index.html']))); self.skipWaiting(); });
+self.addEventListener('activate', e => { self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
